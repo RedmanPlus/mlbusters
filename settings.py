916dfe193be8299d635309012ef05d29a70c972c
Arseny Chebyshev <arseny.chebyshev@gmail.com>
2024-06-11 01:34:50 +0500

    (upd) pick mongo URL from envs

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel, MongoDsn
 
 
 class Settings(BaseModel):
-    mongodb_conn: MongoDsn = "mongodb://user:pass@localhost:27019/?directConnection=true"
+    mongodb_conn: MongoDsn = os.getenv("MONGO_URL", "mongodb://user:pass@localhost:27019/?directConnection=true")
     clip_id: str = os.getenv('CLIP_ID', 'laion/CLIP-ViT-g-14-laion2B-s12B-b42K')
 
 settings = Settings()
