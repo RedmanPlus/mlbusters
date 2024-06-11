e01e12a54e6d618db25f3cce1c7dfc2251fb09f1
Arseny Chebyshev <arseny.chebyshev@gmail.com>
2024-06-12 02:52:52 +0500

    (mvp) Chroma+Clip+API

--- clip.py
+++ clip.py
@@ -15,12 +15,15 @@ class CLIPService:
 
         return Feature(features=features['features'], url=request.video_url)
      
-    async def get_text_embedding(self, request: EncodeRequest) -> Feature:
-        request.text = Translator().translate(text=request.text).text
+    async def get_text_embedding(
+            self, 
+            request: EncodeRequest, 
+            translate: bool = True) -> Feature:
+        if translate:
+            request.text = Translator().translate(text=request.text).text
         async with aiohttp.ClientSession().post(
             self.clip_url, 
             json=request.model_dump(mode="json")
         ) as resp:
             features = await resp.json()
-            print(f"{features=}")
         return Feature(features=features['features'][0])
