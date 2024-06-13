5b98fccd5bc2ecacf9ae17a061bfb6ac80ba2381
Antony Redman <RumataYounger@gmail.com>
2024-06-13 20:37:00 +0300

    finalize dockerfiles and settings

--- main/settings.py
+++ main/settings.py
@@ -7,5 +7,4 @@ env = Env()
 class Settings:
 
     db_port: int = env.int("DB_PORT", default=8080)
-    clip_model: str = env.str("CLIP_MODEL")
     clip_url: str = env.str("CLIP_URL", default="http://localhost:8787/encode")
