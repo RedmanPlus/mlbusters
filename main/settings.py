83a3366396e48f6069a1272ce165cfb200ef3b61
Arseny Chebyshev <arseny.chebyshev@gmail.com>
2024-06-15 01:54:28 +0500

    (upd) LAION CLIP > XCLIP

--- main/settings.py
+++ main/settings.py
@@ -9,4 +9,4 @@ class Settings:
     db_port: int = env.int("DB_PORT", default=8080)
     clip_url: str = env.str("CLIP_URL", default="http://inference:8040/encode")
     memcached_host: str = env.str("MEMCACHED_HOST", default="request_cache")
-    cache_lifetime: int = env.str("CACHE_LIFETIME", default=3600)
+    cache_lifetime: int = env.iny("CACHE_LIFETIME", default=3600)
