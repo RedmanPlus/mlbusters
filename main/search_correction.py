42b3805a97cadce0a35e75f576496bfad6966341
Antony Redman <RumataYounger@gmail.com>
2024-06-16 20:36:55 +0300

    add autoload for suggestions

--- main/search_correction.py
+++ main/search_correction.py
@@ -1,12 +1,18 @@
 from dataclasses import dataclass, field
 
-from autocorrect import Speller
+from autocorrect import Speller, load_from_tar
 
 
 @dataclass
 class SpellCorrection:
 
-    _spell: Speller = field(default_factory=lambda: Speller("ru", fast=True))
+    _spell: Speller = field(
+        default_factory=lambda: Speller(
+            "ru",
+            fast=True,
+            nlp_data=load_from_tar("https://ipfs.io/ipfs/QmbRSZvfJV6zN12zzWhecphcvE9ZBeQdAJGQ9c9ttJXzcg/ru.tar.gz")
+        )
+    )
 
     def __call__(self, search_query: str) -> str:
         return self._spell(search_query)
