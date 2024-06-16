2e94f066f755b314e18258dab90963e26e4b9bec
Antony Redman <RumataYounger@gmail.com>
2024-06-16 20:49:21 +0300

    more suggestion fixing

--- main/search_correction.py
+++ main/search_correction.py
@@ -1,18 +1,12 @@
 from dataclasses import dataclass, field
 
-from autocorrect import Speller, load_from_tar
+from autocorrect import Speller
 
 
 @dataclass
 class SpellCorrection:
 
-    _spell: Speller = field(
-        default_factory=lambda: Speller(
-            "ru",
-            fast=True,
-            nlp_data=load_from_tar("https://ipfs.io/ipfs/QmbRSZvfJV6zN12zzWhecphcvE9ZBeQdAJGQ9c9ttJXzcg/ru.tar.gz")
-        )
-    )
+    _spell: Speller = field(default_factory=lambda: Speller("ru", fast=True))
 
     def __call__(self, search_query: str) -> str:
         return self._spell(search_query)
