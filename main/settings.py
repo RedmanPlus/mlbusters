502aa1251ad941c4a952f1621bc1a33fbd62ed05
Antony Redman <RumataYounger@gmail.com>
2024-06-22 23:40:03 +0300

    change port schematics

--- main/settings.py
+++ main/settings.py
@@ -8,6 +8,6 @@ class Settings:
     db_host: str = env.str("DB_HOST", default="chroma_db")
     db_port: int = env.int("DB_PORT", default=8080)
     encode_clip_url: str = env.str("ENCODE_CLIP_URL", default="http://encode:8040/")
-    search_clip_url: str = env.str("SEARCH_CLIP_URL", default="http://search:8040/")
+    search_clip_url: str = env.str("SEARCH_CLIP_URL", default="http://search:8050/")
     memcached_host: str = env.str("MEMCACHED_HOST", default="request_cache")
     cache_lifetime: int = env.int("CACHE_LIFETIME", default=3600)
