898b711808154f0de0b2b709c4bcff397f2d4c66
Antony Redman <RumataYounger@gmail.com>
2024-06-23 00:21:00 +0300

    add tempfiles for whisper

--- inference/whisper.py
+++ inference/whisper.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass, field
 from io import BytesIO
+import tempfile
 
 import requests
 from whisper_cpp_python import Whisper
@@ -14,5 +15,8 @@ class WhisperService:
     def __call__(self, link: str) -> str:
         
         video_data = BytesIO(requests.get(link).content)
-        data = self._service.transcribe(video_data)
+        with tempfile.NamedTemporaryFile(delete_on_close=False) as tp:
+            tp.write(video_data.read())
+            tp.close()
+            data = self._service.transcribe(open(tp.name))
         return data["text"]
